fbcbb1cf6dd00868a927c365ac45f68c88b6911f
Timofey <timiv2006@gmail.com>
2024-02-25 07:23:27 -0500

    Flask2

--- people/app.py
+++ people/app.py
@@ -5,5 +5,8 @@ def people():
         fake = faker.Faker("ru_RU")
         fake = fake.profile()
         print(fake)
-        text = open("./files/people.txt").write(fake)
+        with open("./files/people.txt", "w", encoding="utf8") as text:
+            for i in range(10):
+                print(fake.name().split(), sep=",", file=text)
+
 people()
