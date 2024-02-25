5e8a6d9aa34a3c0e081929e6ca89c3ee7176097b
Timofey <timiv2006@gmail.com>
2024-02-25 07:40:38 -0500

    Flask2

--- people/app.py
+++ people/app.py
@@ -1,5 +1,13 @@
 import faker
 
+from my_app import app
+from flask import render_template
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/people")
 def people():
     for i in range(10):
         fake = faker.Faker("ru_RU")
@@ -7,6 +15,5 @@ def people():
         print(fake)
         with open("./files/people.txt", "w", encoding="utf8") as text:
             for i in range(10):
-                print(fake.name().split(), sep=",", file=text)
+                print(fake, sep=",", file=text)
 
-people()
