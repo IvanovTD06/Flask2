8d97aa503579ea3c16521f903a1f4f32e25dd9b2
Timofey <timiv2006@gmail.com>
2024-02-25 07:08:51 -0500

    Flask2

--- people/app.py
+++ people/app.py
@@ -1,9 +1,9 @@
 import faker
-from my_app import app
 
-@app.route("/people")
 def people():
     for i in range(10):
         fake = faker.Faker("ru_RU")
         fake = fake.profile()
+        print(fake)
         text = open("./files/people.txt").write(fake)
+people()
